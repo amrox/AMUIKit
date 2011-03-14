52472223264a841eae3b9d7c3443ef68da9de1e3
Andy Mroczkowski <andy@mrox.net>
2011-03-14 06:52:20 -0500

    Added base files

---
+++ Base/UIColor+AMMoreColors.m
@@ -0,0 +1,20 @@
+//
+//  UIColor+MoreColors.m
+//  SoleSearch
+//
+//  Created by Andy Mroczkowski on 7/12/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIColor+AMMoreColors.h"
+
+
+@implementation UIColor (AMMoreColors)
+
++ (UIColor*) am_standardBlueButtonTextColor
+{
+	return [UIColor colorWithRed:36.0/255.0 green:60.0/255.0 blue:116.0/255.0 alpha:1.0];
+}
+
+
+@end
